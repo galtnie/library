15c28a3e362292fc81eb083eed115f66d4adc6df
galtnie <hereby@protonmail.com>
2019-07-28 22:51:15 +0300

    Updates

---
+++ precache-manifest.efc113825e2c1a772009a97ae25bcd1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9d953f952da2553f9f8ae7535a9dd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8421821b575a24f524b4",
+    "url": "/static/js/2.3ae5b3f8.chunk.js"
+  },
+  {
+    "revision": "94f28c55c01f29246f6b",
+    "url": "/static/js/main.6cc35298.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5434740cfd131adef8acbdf8fb43ba3a",
+    "url": "/static/media/arrow_left.5434740c.svg"
+  }
+]);
