416ee40c5d994a1a3623e138e702f1aa8b7c8595
galtnie <hereby@protonmail.com>
2019-07-28 23:07:17 +0300

    change router

--- src/App.js
+++ src/App.js
@@ -1,75 +1,38 @@
 import React from "react";
-import {
-  MuiThemeProvider,
-  createMuiTheme,
-  CssBaseline,
-  Container,
-} from "@material-ui/core";
-import { deepPurple, indigo, grey } from "@material-ui/core/colors";
-import { Provider } from "react-redux";
-import store from "./store";
+import { Container } from "@material-ui/core";
 import { ButtonList, Inclusion, Information, Completion } from "./components";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { StyledPaper } from "./styles";
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: indigo[500],
-    },
-    secondary: {
-      main: deepPurple[500],
-    },
-    background: {
-      paper: "#fff",
-      default: grey[100],
-    },
-  },
-  "@global": {
-    "html, body, #root": {
-      width: "100%",
-    },
-  },
-});
 
 function App() {
   return (
-    <Provider store={store}>
-      <MuiThemeProvider theme={theme}>
-        <CssBaseline />
-        <Container maxWidth="md">
-          <StyledPaper component="div" elevation={2}>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
-              <Route
-                path="/"
-                exact
-                render={props => <ButtonList {...props} section={"genres"} />}
-              />
-              <Route
-                path="/subgenres"
-                render={props => (
-                  <ButtonList {...props} section={"subgenres"} />
-                )}
-              />
-              <Route
-                path="/genre"
-                render={props => <Inclusion {...props} section={"genre"} />}
-              />
-              <Route
-                path="/subgenre"
-                render={props => <Inclusion {...props} section={"subgenre"} />}
-              />
-              <Route
-                path="/information"
-                component={Information}
-                section={"information"}
-              />
-              <Route path="/completion" component={Completion} />
-            </BrowserRouter>
-          </StyledPaper>
-        </Container>
-      </MuiThemeProvider>
-    </Provider>
+    <Container maxWidth="md">
+      <StyledPaper component="div" elevation={2}>
+        <Route
+          path="/"
+          exact
+          render={props => <ButtonList {...props} section={"genres"} />}
+        />
+        <Route
+          path="/subgenres"
+          render={props => <ButtonList {...props} section={"subgenres"} />}
+        />
+        <Route
+          path="/genre"
+          render={props => <Inclusion {...props} section={"genre"} />}
+        />
+        <Route
+          path="/subgenre"
+          render={props => <Inclusion {...props} section={"subgenre"} />}
+        />
+        <Route
+          path="/information"
+          component={Information}
+          section={"information"}
+        />
+        <Route path="/completion" component={Completion} />
+      </StyledPaper>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import {
+  MuiThemeProvider,
+  CssBaseline,
+  createMuiTheme,
+} from "@material-ui/core";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter } from "react-router-dom";
+import { deepPurple, indigo, grey } from "@material-ui/core/colors";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: indigo[500],
+    },
+    secondary: {
+      main: deepPurple[500],
+    },
+    background: {
+      paper: "#fff",
+      default: grey[100],
+    },
+  },
+  "@global": {
+    "html, body, #root": {
+      width: "100%",
+    },
+  },
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </MuiThemeProvider>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
